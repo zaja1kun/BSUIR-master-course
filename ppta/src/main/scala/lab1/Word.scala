f11b9537766cd2f1291d3bb69421cc6976dea414
vladkanash <vlad.kanash@gmail.com>
2017-11-22 01:17:47 +0300

    Word trait extended.

--- ppta/src/main/scala/lab1/Word.scala
+++ ppta/src/main/scala/lab1/Word.scala
@@ -1,10 +1,20 @@
 package lab1
 
-sealed trait Word
+sealed trait Word {
+  def len: Int
+  def contents: Set[Symbol]
+}
 
-case class EmptyWord() extends Word
+case class EmptyWord() extends Word {
+  override val len = 0
+  override val contents: Set[Nothing] = Set.empty
+}
+
 case class SymbolicWord(value: CharSequence) extends Word {
   require(value.length() > 0, "word cannot be null")
+
+  override val len: Int = value.length()
+  override val contents: CharSequence = value
 
   override def toString: String = value.toString
 }
