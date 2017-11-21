456b540c8075c656a4b1e0de02b899dea53473fc
vladkanash <vlad.kanash@gmail.com>
2017-11-22 01:17:22 +0300

    Added grammar type logic.

--- ppta/src/main/scala/lab1/Grammar.scala
+++ ppta/src/main/scala/lab1/Grammar.scala
@@ -1,16 +1,20 @@
 package lab1
 
-class Grammar(terminals: Set[Symbol],
-              nonTerminals: Set[Symbol],
-              rules: Set[Rule],
-              startSymbol: Symbol) {
+class Grammar(val terminals: Set[Symbol],
+              val nonTerminals: Set[Symbol],
+              val rules: Set[Rule],
+              val startSymbol: Symbol) {
 
   require((terminals & nonTerminals & Set(startSymbol)).isEmpty, "Duplicate symbols found")
+
+  lazy val `type`: GrammarType.Value = GrammarType.Type0
+
+  def isContextFree: Boolean = rules forall
+    (rule => rule.left.len == 1 && (rule.left.contents -- nonTerminals).isEmpty)
 
   override def toString: String =
     s"""Terminals: ${terminals.mkString}
       |NonTerminals: ${nonTerminals.mkString}
       |Rules: ${rules.mkString("\n\t", ";\n\t", "")}
       |Start symbol: $startSymbol""".stripMargin
-
 }
---
+++ ppta/src/main/scala/lab1/GrammarType.scala
@@ -0,0 +1,7 @@
+package lab1
+
+object GrammarType extends Enumeration {
+  type WeekDay = Value
+  val Type0, ContextFree, ContextDependent, Regular = Value
+}
+
