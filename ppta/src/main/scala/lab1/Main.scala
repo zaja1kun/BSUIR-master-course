4009a412980d5e454b64e8bcff14f10631210330
Vlad Kanash <vlad.kanash@expert-soft.by>
2017-11-21 23:30:46 +0300

    main updated to read from file.

--- ppta/src/main/scala/lab1/Main.scala
+++ ppta/src/main/scala/lab1/Main.scala
@@ -1,8 +1,12 @@
 package lab1
 
+import java.io.FileReader
+
+import lab1.parser.GrammarParser
+
 object Main {
   def main(args: Array[String]): Unit = {
-    print(RuleParser.parse(args(0)))
+    print(GrammarParser.parse(new FileReader(args(0))))
 
   }
 
